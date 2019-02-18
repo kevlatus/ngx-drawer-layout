e603cda8fe95048206036ccb5602d058be1af443
Kevin Latusinski <kevin.latusinski@gmail.com>
2019-02-18 21:05:45 +0100

    Fix file copy during publish process

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const exec = require('child_process').exec;
-const {LIB_DIST_FOLDER, LIB_SOURCE_FOLDER, LIB_PUBLIC_NAME, LIB_VERSION, ROOT_PATH} = require('./utils');
+const {LIB_DIST_FOLDER, LIB_PUBLIC_NAME, LIB_VERSION, ROOT_PATH} = require('./utils');
 
 const LIB_PACKAGE_NAME = `${LIB_PUBLIC_NAME}-${LIB_VERSION}.tgz`;
 const LICENSE_PATH = path.resolve(ROOT_PATH, 'LICENSE');
 const README_PATH = path.resolve(ROOT_PATH, 'README.md');
 
-fs.copyFileSync(LICENSE_PATH, path.resolve(LIB_SOURCE_FOLDER, 'LICENSE'));
-fs.copyFileSync(README_PATH, path.resolve(LIB_SOURCE_FOLDER, 'README.md'));
+fs.copyFileSync(LICENSE_PATH, path.resolve(LIB_DIST_FOLDER, 'LICENSE'));
+fs.copyFileSync(README_PATH, path.resolve(LIB_DIST_FOLDER, 'README.md'));
 
 exec(
   `npm pack`,

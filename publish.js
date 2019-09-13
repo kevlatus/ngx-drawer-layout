7549400fc310af478f63609578d835e46af2f9e0
Kevin Latusinski <kevin.latusinski@gmail.com>
2019-09-13 17:06:48 +0200

    build: simplify publish script

--- publish.js
+++ publish.js
@@ -1,7 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 const exec = require('child_process').exec;
-const {LIB_DIST_FOLDER, LIB_PUBLIC_NAME, LIB_VERSION, ROOT_PATH} = require('./utils');
+
+const ROOT_PATH = path.resolve(__dirname);
+const DIST_PATH = path.resolve(ROOT_PATH, 'dist');
+const PROJECTS_PATH = path.resolve(ROOT_PATH, 'projects');
+
+const COMPANION_APP_NAME = require(path.resolve(ROOT_PATH, 'package.json')).name;
+const LIB_PROJECT_NAME = COMPANION_APP_NAME.replace('-app', '');
+const LIB_PUBLIC_NAME = 'ngx-' + LIB_PROJECT_NAME;
+const LIB_SOURCE_FOLDER = path.resolve(PROJECTS_PATH, LIB_PROJECT_NAME);
+const LIB_DIST_FOLDER = path.resolve(DIST_PATH, LIB_PROJECT_NAME);
+const LIB_VERSION = require(path.resolve(LIB_SOURCE_FOLDER, 'package.json')).version;
 
 const LIB_PACKAGE_NAME = `${LIB_PUBLIC_NAME}-${LIB_VERSION}.tgz`;
 const LICENSE_PATH = path.resolve(ROOT_PATH, 'LICENSE');

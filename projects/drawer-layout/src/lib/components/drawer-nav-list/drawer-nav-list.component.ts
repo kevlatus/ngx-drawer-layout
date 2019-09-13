4bcabc1cf7d74c4cdd209cbb1c9fcc68e5bcd9ff
Kevin Latusinski <kevin.latusinski@gmail.com>
2019-09-13 17:58:44 +0200

    feat: close drawer on NavItem click on mobile

--- projects/drawer-layout/src/lib/components/drawer-nav-list/drawer-nav-list.component.ts
+++ projects/drawer-layout/src/lib/components/drawer-nav-list/drawer-nav-list.component.ts
@@ -1,6 +1,8 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, ElementRef} from '@angular/core';
+import {take} from 'rxjs/operators';
 
 import {DrawerNavItem} from '../../models/drawer-nav';
+import {DrawerService} from '../../services/drawer.service';
 
 /**
  * This component renders all drawer items given by {@link items}.
@@ -12,6 +14,38 @@ import {DrawerNavItem} from '../../models/drawer-nav';
   styleUrls: ['./drawer-nav-list.component.scss']
 })
 export class DrawerNavListComponent {
+  private drawerType: 'start' | 'end' | undefined;
+
   @Input() public heading: string;
   @Input() public items: DrawerNavItem[] = [];
+
+  private findDrawerType() {
+    let it = this.elementRef.nativeElement;
+    while (it) {
+      if (it.hasAttribute('ngxdrawercontent')) {
+        return 'start';
+      } else if (it.hasAttribute('ngxenddrawercontent')) {
+        return 'end';
+      }
+
+      it = it.parentElement;
+    }
+  }
+
+  constructor(private elementRef: ElementRef<HTMLElement>, private drawer: DrawerService) {
+    this.drawerType = this.findDrawerType();
+  }
+
+  onItemClicked() {
+    if (!this.drawerType) {
+      return;
+    }
+
+    const drawerController = this.drawerType === 'start' ? this.drawer.start : this.drawer.end;
+    drawerController.mode$.pipe(take(1)).subscribe(mode => {
+      if (mode === 'over') {
+        drawerController.close();
+      }
+    });
+  }
 }

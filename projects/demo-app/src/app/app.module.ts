a1cf5e46b1b47c2256398240c0f8f616e78eca87
Kevin Latusinski <9928134+kevlatus@users.noreply.github.com>
2020-09-06 08:04:40 +0000

    feat(demo): add links to github and npm to header

--- projects/demo-app/src/app/app.module.ts
+++ projects/demo-app/src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DrawerLayoutModule } from 'ngx-drawer-layout';
 import { DrawerNavModule } from 'ngx-drawer-layout/nav';
@@ -28,6 +29,7 @@ import { DrawerConfigFormComponent } from './drawer-config-form/drawer-config-fo
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    HttpClientModule,
     MatButtonToggleModule,
     MatCardModule,
     MatSlideToggleModule,
@@ -41,4 +43,16 @@ import { DrawerConfigFormComponent } from './drawer-config-form/drawer-config-fo
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(matIconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    matIconRegistry.addSvgIcon(
+      'github',
+      sanitizer.bypassSecurityTrustResourceUrl('/assets/ic-github.svg')
+    );
+
+    matIconRegistry.addSvgIcon(
+      'npm',
+      sanitizer.bypassSecurityTrustResourceUrl('/assets/ic-npm.svg')
+    );
+  }
+}
